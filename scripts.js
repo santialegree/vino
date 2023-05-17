b6df3214ed7b2b5be034d45330974f1bbd4f60e5
= <=>
2023-05-17 13:47:40 -0300

    tercera

--- scripts.js
+++ scripts.js
@@ -33,5 +33,22 @@ $(document).ready(function(){
       },
     ],
   }); 
-    /* funcionalidad del carrousel de los shows */
+    /* funcionalidad del botona rriba*/
 
+   // Cuando el usuario hace scroll hacia abajo 20px desde la parte superior de la página, muestra el botón
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// Cuando el usuario hace clic en el botón, vuelve a la parte superior de la página
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+    
