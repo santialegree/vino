7deeac13c8a736b521dc1aabe237ff2dc9efd951
= <=>
2023-05-17 12:31:37 -0300

    segundo

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.photo-carousel').slick({
       autoplay: true, // Activar el desplazamiento automático
       autoplaySpeed: 3000, // Velocidad del desplazamiento automático en milisegundos
-      dots: true, // Agregar indicadores de navegación (puntos)
-      arrows: true, // Agregar botones de navegación (flechas)
+      /* dots: true, // Agregar indicadores de navegación (puntos)
+      arrows: true, // Agregar botones de navegación (flechas) */
       infinite: true, // Desplazarse infinitamente
       speed: 500, // Velocidad de transición en milisegundos
       slidesToShow: 1, // Mostrar una imagen por vez
